1dc3ec879d0fa0a773e745126ad00b5601d931af
Penelope A <penelope.arnold@du.edu>
2019-11-09 20:47:11 -0700

    final changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,37 @@
 $(document).ready(function() {
 
-
 var yourNum = 0
 
 var randomNum = randomGenerated();
 
 var wins = 0
 var loss = 0
-var crystals;
+var crystals = crystalNum();
 
 function crystalNum (){
 return{
     red:{
         points: Math.floor(Math.random() * 12) + 1,
-        imageUrl: "assets/images/red.png"
+        imageUrl: "/Users/PenelopeArnold/Desktop/bootcamp/homework/unit-4-game/assets/images/red.png"
 
     },
     yellow:{
         points: Math.floor(Math.random() * 12) + 1,
-        imageUrl: "assets/images/yellow.png"
+        imageUrl: "/Users/PenelopeArnold/Desktop/bootcamp/homework/unit-4-game/assets/images/yellow.png"
     },
     blue:{
         points: Math.floor(Math.random() * 12) + 1,
-        imageUrl: "assets/images/blue.png"
+        imageUrl: "/Users/PenelopeArnold/Desktop/bootcamp/homework/unit-4-game/assets/images/blue.png"
     },
     green:{
         points: Math.floor(Math.random() * 12) + 1,
-        imageUrl: "assets/images/green.png"
+        imageUrl: "/Users/PenelopeArnold/Desktop/bootcamp/homework/unit-4-game/assets/images/green.png"
     }
 }
 }
 
-function randomNum (){
+
+function randomGenerated (){
     return Math.floor(Math.random()*102)+19
 }
 
@@ -41,9 +41,9 @@ function set(){
     randomNum = randomGenerated();
     $("#random").text(randomNum);
 }
-set();
+// set();
 
-function page (){
+function page (userWins){
     $("#win-loss").empty();
     if (userWins === true){
         $("#win-loss").append($("<p>").text("Winner!"))
@@ -55,74 +55,65 @@ function page (){
     }
 
     var wSpan = $("<span>").text(wins)
-    var lSpan = $("<span>").text(losses)
+    var lSpan = $("<span>").text(loss)
 
-    pWins.append(wSpan);
-    pLosses.append(lSpan);
+
 
     var pWins = $("<p>").text("Wins: ")
     var pLosses= $("<p>").text("Losses: ")
 
+    pWins.append(wSpan);
+    pLosses.append(lSpan);
+
+
     $("#win-loss").append(pWins)
     $("#win-loss").append(pLosses)
 }
-page();
+//page();
 
 function renderCrystals(){
     for (var key in crystals){
-        var crystalDiv = $("<div class=`crystal-button` data-name='" + key + "'>");
-        var crystalImage = $("<img alt=`crystal` class=`crystal-image`>").attr(src, crystals[key].imageUrl);
+        var crystalDiv = $("<div class='crystals-button' data-name='" + key + "'>");
+        var crystalImage = $("<img alt=`crystal` class=`crystal-image`>").attr("src", crystals[key].imageUrl);
         crystalDiv.append(crystalImage);
         $("#crystal-div").append(crystalDiv);
     }
-}renderCrystals();
-
-function match(){
-    yourNum += crystals[crystals.attr("data-name")].points;
 }
-match();
+
+function match(crystals){
+    yourNum +=  crystals[crystals.attr('data-name')].points;
+}
+
 
 function renderMatch(){
-    var scoreNum = $("<div id=`points`>").text(yourNum);
+    var score = $("<div id=`points`>").text(yourNum);
     $("#score").html();
-    $("#score").html(scoreNum)
+    $("#score").html(score)
 }
+//renderMatch();
+
+set();
+page();
+renderCrystals();
 renderMatch();
 
+$('.crystals-button').on("click", function(event){
+    console.log("click")
+    match($(this));
+    renderMatch();
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if (yourNum === randomNum){
+        wins++;
+        set();
+        page(true);
+    } 
+    
+    else if (yourNum > randomNum ){
+        loss++;
+        set();
+        page(false);
+    }
+})
 
 })
